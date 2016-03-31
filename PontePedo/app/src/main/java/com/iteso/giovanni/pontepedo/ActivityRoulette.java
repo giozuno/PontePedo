9d9361052eed4b7b5853129b08bce73a567f981d
GIOVANNI <giovannizunoalcazar@gmail.com>
2016-03-31 12:52:43 -0600

    Ruleta con animacion completada y cambio de titulo y sonido ganador al segundo click

--- PontePedo/app/src/main/java/com/iteso/giovanni/pontepedo/ActivityRoulette.java
+++ PontePedo/app/src/main/java/com/iteso/giovanni/pontepedo/ActivityRoulette.java
@@ -2,9 +2,10 @@ package com.iteso.giovanni.pontepedo;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,13 +13,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
 public class ActivityRoulette extends AppCompatActivity {
-    String reto;
-
+    String challenge;
+    AnimationDrawable animation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,58 +26,53 @@ public class ActivityRoulette extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        final ImageButton roulette = (ImageButton) findViewById(R.id.roulette);
         final TextView tileRoulette = (TextView) findViewById(R.id.title_roulette);
         Button button = (Button) findViewById(R.id.buttonRoulette);
+        final ImageButton roulette = (ImageButton) findViewById(R.id.roulette);
         roulette.setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
+                int time = 300;
+                animation = new AnimationDrawable();
+                fullRoulette(time);
                 Random rnd = new Random();
-                int numRnd = (int)(rnd.nextDouble() * 16 + 8);
-                int x=0, i;
-                for(i=0; i<numRnd; i++) {
-                    delay(1);
-                    x = i;
-                    while(x>7)
-                        x -= 8;
-                    switch (x) {
-                        case 0:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta1));
-                            reto = getString(R.string.reto1);
-                            break;
-                        case 1:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta2));
-                            reto = getString(R.string.reto2);
-                            break;
-                        case 2:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta3));
-                            reto = getString(R.string.reto3);
-                            break;
-                        case 3:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta4));
-                            reto = getString(R.string.reto4);
-                            break;
-                        case 4:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta5));
-                            reto = getString(R.string.reto5);
-                            break;
-                        case 5:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta6));
-                            reto = getString(R.string.reto6);
-                            break;
-                        case 6:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta7));
-                            reto = getString(R.string.reto7);
-                            break;
-                        case 7:
-                            roulette.setImageDrawable(getDrawable(R.drawable.ruleta8));
-                            reto = getString(R.string.reto8);
-                            break;
-                    }
+                int numRnd = (int)(rnd.nextDouble() * 7);
+                switch (numRnd) {
+                    case 0:
+                        roulette1(time);
+                        break;
+                    case 1:
+                        roulette2(time);
+                        break;
+                    case 2:
+                        roulette3(time);
+                        break;
+                    case 3:
+                        roulette4(time);
+                    case 4:
+                        roulette5(time);
+                        break;
+                    case 5:
+                        roulette6(time);
+                        break;
+                    case 6:
+                        roulette7(time);
+                        break;
+                    case 7:
+                        roulette8(time);
+                        break;
                 }
-                tileRoulette.setText(reto);
-                Toast.makeText(getApplicationContext(), "i: " + i + "   x: " + x + "   rnd: " + numRnd, Toast.LENGTH_SHORT).show();
+                roulette.setImageDrawable(animation);
+                animation.start();
+                roulette.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        tileRoulette.setText(challenge);
+                        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.ding_ganador);
+                        mp.start();
+                    }
+                });
             }
         });
         button.setOnClickListener(new View.OnClickListener() {
@@ -111,12 +106,64 @@ public class ActivityRoulette extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void delay(int sec){
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-            }
-        }, sec * 1000);
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void fullRoulette(int sec) {
+        this.animation.setOneShot(true);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta1), sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta2), sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta3), sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta4), sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta5), sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta6), sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta7), sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta8), sec);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette1(int sec){
+        this.animation.addFrame(getDrawable(R.drawable.ruleta1), sec);
+        challenge = getString(R.string.reto1);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette2(int sec){
+        roulette1(sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta2), sec);
+        challenge = getString(R.string.reto2);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette3(int sec){
+        roulette2(sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta3), sec);
+        challenge = getString(R.string.reto3);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette4(int sec){
+        roulette3(sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta4), sec);
+        challenge = getString(R.string.reto4);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette5(int sec){
+        roulette4(sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta5), sec);
+        challenge = getString(R.string.reto5);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette6(int sec){
+        roulette5(sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta6), sec);
+        challenge = getString(R.string.reto6);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette7(int sec){
+        roulette6(sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta7), sec);
+        challenge = getString(R.string.reto7);
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void roulette8(int sec){
+        roulette7(sec);
+        this.animation.addFrame(getDrawable(R.drawable.ruleta8), sec);
+        challenge = getString(R.string.reto8);
     }
 }
