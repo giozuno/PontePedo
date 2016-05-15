f66a3abfa7b119165272d3931be899d3129de6df
GIOVANNI <giovannizunoalcazar@gmail.com>
2016-05-14 19:05:56 -0500

    App 2.1 Aplicacion con animaciones de CardBack. Boton de Facebook Implementado

--- PontePedo/app/src/main/java/com/iteso/giovanni/pontepedo/ActivityMain.java
+++ PontePedo/app/src/main/java/com/iteso/giovanni/pontepedo/ActivityMain.java
@@ -1,12 +1,15 @@
 package com.iteso.giovanni.pontepedo;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ActivityMain extends AppCompatActivity {
     @Override
@@ -19,6 +22,7 @@ public class ActivityMain extends AppCompatActivity {
 
         final Button play = (Button) findViewById(R.id.menu_button_play);
         final Button games = (Button) findViewById(R.id.menu_button_games);
+        final ImageButton facebook = (ImageButton) findViewById(R.id.menu_button_facebook);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +37,12 @@ public class ActivityMain extends AppCompatActivity {
                 gamesIntent.putExtra("edit", false);
                 gamesIntent.putExtra("gameOld", -1);
                 startActivity(gamesIntent);
+            }
+        });
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(getOpenFacebookIntent(getApplicationContext()));
             }
         });
     }
@@ -57,5 +67,14 @@ public class ActivityMain extends AppCompatActivity {
         });
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    public static Intent getOpenFacebookIntent(Context context) {
+        try {
+            context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/885386441584276"));
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/GameOnTheApp"));
+        }
     }
 }
